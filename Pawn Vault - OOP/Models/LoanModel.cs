802b22f3ab01a28d2d27d8b71f6fa0e49d2ea100
Kenneth James Boncales <kennethboncales608@gmail.com>
2025-06-16 10:37:47 +0800

    Added Loan Model

---
+++ Pawn Vault - OOP/Models/LoanModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pawn_Vault___OOP.Models
+{
+    public class LoanModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ItemName { get; set; } 
+        public decimal Amount { get; set; }
+        public string Status { get; set; }
+        public DateTime IssuedDate { get; set; } = DateTime.UtcNow;
+        public decimal InterestRate { get; set; }
+        public DateOnly RetentionPeriod { get; set; }
+
+        // === Foreign Keys, tho sitll not sure kung need pa ba ideclare or not ===
+        /*  
+        public int EmployeeId { get; set; }
+        public int CustomerId { get; set; }
+        */
+
+    }
+}
