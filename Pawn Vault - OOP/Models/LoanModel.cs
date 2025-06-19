402456e1217bed861e9e92ae7feb8be921f945c1
Kenneth James Boncales <kennethboncales608@gmail.com>
2025-06-19 23:59:03 +0800

    added description attribute, foreign key and navigation property for linking two tables.

--- Pawn Vault - OOP/Models/LoanModel.cs
+++ Pawn Vault - OOP/Models/LoanModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace Pawn_Vault___OOP.Models
 {
     public class LoanModel
@@ -9,6 +8,9 @@ namespace Pawn_Vault___OOP.Models
         [Required]
         [StringLength(100)]
         public string ItemName { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
 
         [Required]
         public decimal Amount { get; set; }
@@ -25,11 +27,14 @@ namespace Pawn_Vault___OOP.Models
         [Required]
         public DateOnly RetentionPeriod { get; set; }
 
-        // === Foreign Keys, tho sitll not sure kung need pa ba ideclare or not ===
-        /*  
-        public int EmployeeId { get; set; }
+        // === Foreign Key ===
         public int CustomerId { get; set; }
-        */
+
+        // ====== Navigation Property, para ma link yung Loan at Customer Table at makuha yung Customer Name ======
+        // public Customer Customer { get; set;} // di pa naiimplement ung table ng Customer kaya di muna siya isasama.
+
+        // === sitll not sure kung need pa ba ideclare or not ===
+        // public int EmployeeId { get; set; 
 
     }
 }
