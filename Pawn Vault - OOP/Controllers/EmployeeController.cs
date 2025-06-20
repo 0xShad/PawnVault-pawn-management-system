3a5d6008ae9556db9aaebe24f9350131b38087d5
Prince Aron Garcia <studentprinsipe@gmail.com>
2025-06-20 16:39:11 +0800

    updated employee controller & updated db

--- Pawn Vault - OOP/Controllers/EmployeeController.cs
+++ Pawn Vault - OOP/Controllers/EmployeeController.cs
@@ -1,12 +1,100 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// EmployeeController.cs
+using Microsoft.AspNetCore.Mvc;
+using Pawn_Vault___OOP.Data;
+using Pawn_Vault___OOP.Models;
+using System.Linq;
 
 namespace Pawn_Vault___OOP.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employee
         public IActionResult Index()
         {
+            var employees = _context.Employees.ToList();
+            return View(employees);
+        }
+
+        // GET: Employee/Create
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Edit/5
+        public IActionResult Edit(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Employee employee)
+        {
+            if (id != employee.EmployeeID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(employee);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+        // GET: Employee/Delete/5
+        public IActionResult Delete(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var employee = _context.Employees.Find(id);
+            if (employee != null)
+            {
+                _context.Employees.Remove(employee);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- Pawn Vault - OOP/Data/Migrations/20250620082619_EmployeeCreate.Designer.cs
+++ Pawn Vault - OOP/Data/Migrations/20250620082619_EmployeeCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pawn_Vault___OOP.Data;
 
@@ -11,9 +12,11 @@ using Pawn_Vault___OOP.Data;
 namespace Pawn_Vault___OOP.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250620082619_EmployeeCreate")]
+    partial class EmployeeCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
