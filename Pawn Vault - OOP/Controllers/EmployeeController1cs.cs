74e83d8773c947d548fba7f2470c99851c860848
Prince Aron Garcia <studentprinsipe@gmail.com>
2025-06-24 20:04:33 +0800

    with employee query, using identity

---
+++ Pawn Vault - OOP/Controllers/EmployeeController1cs.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pawn_Vault___OOP.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public EmployeeController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var staffUsers = await _userManager.GetUsersInRoleAsync("Staff");
+            return View(staffUsers); // expects Views/Employee/Index.cshtml
+        }
+    }
+}
